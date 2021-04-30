5a084cd45c372300dcd964bd7588e8030bbc478b
1KimJiHyeon <mycasio1@mju.ac.kr>
2021-04-30 23:54:17 +0900

    error 수정

--- app/src/main/java/com/example/woohangsi_app/Alarm.java
+++ app/src/main/java/com/example/woohangsi_app/Alarm.java
@@ -23,7 +23,7 @@ public class Alarm extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.alarm);
 
     Button button = (Button) findViewById(R.id.button);
     button.setOnClickListener(new View.OnClickListener() {
