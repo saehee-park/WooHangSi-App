1107b0df24f3d48fe86f44c900b1dcedb42a15a6
1KimJiHyeon <mycasio1@mju.ac.kr>
2021-05-01 04:19:48 +0900

    카테고리별 예산 업데이트

--- app/src/main/java/com/example/woohangsi_app/BudgetCategory.java
+++ app/src/main/java/com/example/woohangsi_app/BudgetCategory.java
@@ -1,4 +1,11 @@
 package com.example.woohangsi_app;
 
-public class BudgetCategory {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BudgetCategory extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category_budget);}
 }
