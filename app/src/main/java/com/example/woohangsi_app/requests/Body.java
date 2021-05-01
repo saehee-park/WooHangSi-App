fb76a2fc4bffaf3fed4153890026af73870e84c1
Dongjun Jeong <dkdlel1087@naver.com>
2021-05-01 15:15:43 +0900

    connect DB

--- app/src/main/java/com/example/woohangsi_app/requests/Body.java
+++ app/src/main/java/com/example/woohangsi_app/requests/Body.java
@@ -5,7 +5,25 @@ public class Body {
 
   public Body() {
     this.getIndivAllAccInfo = "{\"dataHeader\": {\"UTZPE_CNCT_IPAD\": \"\",\"UTZPE_CNCT_MCHR_UNQ_ID\": \"\",\"UTZPE_CNCT_TEL_NO_TXT\": \"\",\"UTZPE_CNCT_MCHR_IDF_SRNO\": \"\",\"UTZ_MCHR_OS_DSCD\": \"\",\"UTZ_MCHR_OS_VER_NM\": \"\",\"UTZ_MCHR_MDL_NM\": \"\",\"UTZ_MCHR_APP_VER_NM\": \"\" },\"dataBody\": {} }";
-    this.getAccBasicInfo = "{\"dataHeader\": {\"UTZPE_CNCT_IPAD\": \"\",\"UTZPE_CNCT_MCHR_UNQ_ID\": \"\",\"UTZPE_CNCT_TEL_NO_TXT\": \"\",\"UTZPE_CNCT_MCHR_IDF_SRNO\": \"\",\"UTZ_MCHR_OS_DSCD\": \"\",\"UTZ_MCHR_OS_VER_NM\": \"\",\"UTZ_MCHR_MDL_NM\": \"\",\"UTZ_MCHR_APP_VER_NM\": \"\" },\"dataBody\": {\"INQ_ACNO\": \"1002123456789\",\"INQ_BAS_DT\": \"20210220\",\"ACCT_KND\": \"P\",\"INQ_CUCD\": \"KRW\" } }";
+//    this.getAccBasicInfo = "{\"dataHeader\": {\"UTZPE_CNCT_IPAD\": \"\",\"UTZPE_CNCT_MCHR_UNQ_ID\": \"\",\"UTZPE_CNCT_TEL_NO_TXT\": \"\",\"UTZPE_CNCT_MCHR_IDF_SRNO\": \"\",\"UTZ_MCHR_OS_DSCD\": \"\",\"UTZ_MCHR_OS_VER_NM\": \"\",\"UTZ_MCHR_MDL_NM\": \"\",\"UTZ_MCHR_APP_VER_NM\": \"\" },\"dataBody\": {\"INQ_ACNO\": \"1002123456789\",\"INQ_BAS_DT\": \"20210220\",\"ACCT_KND\": \"P\",\"INQ_CUCD\": \"KRW\" } }";
+    this.getAccBasicInfo = "{\n" +
+            "\"dataHeader\": {\n" +
+            "\"UTZPE_CNCT_IPAD\": \"10.0.0.1\",\n" +
+            "\"UTZPE_CNCT_MCHR_UNQ_ID\": \"3B5E6E7B\",\n" +
+            "\"UTZPE_CNCT_TEL_NO_TXT\": \"01012341234\",\n" +
+            "\"UTZPE_CNCT_MCHR_IDF_SRNO\": \"IMEI\",\n" +
+            "\"UTZ_MCHR_OS_DSCD\": \"1\",\n" +
+            "\"UTZ_MCHR_OS_VER_NM\": \"8.0.0\",\n" +
+            "\"UTZ_MCHR_MDL_NM\": \"SM-G930S\",\n" +
+            "\"UTZ_MCHR_APP_VER_NM\": \"1.0.0\"\n" +
+            "},\n" +
+            "\"dataBody\": {\n" +
+            "\"INQ_ACNO\": \"1002123456789\",\n" +
+            " \"INQ_BAS_DT\": \"20190101\",\n" +
+            " \"INQ_CUCD\": \"USD\",\n" +
+            "\"ACCT_KND\": \"P\"\n" +
+            "}\n" +
+            "}";
     this.getAccTransList = "{\"dataHeader\": {\"UTZPE_CNCT_IPAD\": \"\",\"UTZPE_CNCT_MCHR_UNQ_ID\": \"\",\"UTZPE_CNCT_TEL_NO_TXT\": \"\",\"UTZPE_CNCT_MCHR_IDF_SRNO\": \"\",\"UTZ_MCHR_OS_DSCD\": \"\",\"UTZ_MCHR_OS_VER_NM\": \"\",\"UTZ_MCHR_MDL_NM\": \"\",\"UTZ_MCHR_APP_VER_NM\": \"\" },\"dataBody\": {\"INQ_ACNO\": \"1002123456789\",\"INQ_STA_DT\": \"20210101\",\"INQ_END_DT\": \"20210310\",\"NEW_DT\": \"20140522\",\"ACCT_KND\": \"P\",\"CUCD\": \"KRW\" } }";
     this.getCellCerti = "{\"dataHeader\": {\"UTZPE_CNCT_IPAD\": \"\",\"UTZPE_CNCT_MCHR_UNQ_ID\": \"\",\"UTZPE_CNCT_TEL_NO_TXT\": \"\",\"UTZPE_CNCT_MCHR_IDF_SRNO\": \"\",\"UTZ_MCHR_OS_DSCD\": \"\",\"UTZ_MCHR_OS_VER_NM\": \"\",\"UTZ_MCHR_MDL_NM\": \"\",\"UTZ_MCHR_APP_VER_NM\": \"\" },\"dataBody\": {\"COMC_DIS\": \"1\",\"HP_NO\": \"01012345678\",\"HP_CRTF_AGR_YN\": \"Y\",\"FNM\": \"홍길동\",\"RRNO_BFNB\": \"930216\",\"ENCY_RRNO_LSNM\": \"1234567\" } }";
 //    this.getCellCerti = "{\n" +
@@ -34,7 +52,7 @@ public class Body {
             "\"UTZPE_CNCT_MCHR_UNQ_ID\": \"3B5E6E7B\",\n" +
             "\"UTZPE_CNCT_TEL_NO_TXT\": \"01012341234\",\n" +
             "\"UTZPE_CNCT_MCHR_IDF_SRNO\": \"IMEI\",\n" +
-            "\"UTZ_MCHR_OS_DSCD\": \"1\",페이지 15 / 79\n" +
+            "\"UTZ_MCHR_OS_DSCD\": \"1\",\n" +
             "\"UTZ_MCHR_OS_VER_NM\": \"8.0.0\",\n" +
             "\"UTZ_MCHR_MDL_NM\": \"SM-G930S\",\n" +
             "\"UTZ_MCHR_APP_VER_NM\": \"1.0.0\"\n" +
