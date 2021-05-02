46baf7b7badcab8aca9bcda0096292c80a9668fa
1KimJiHyeon <mycasio1@mju.ac.kr>
2021-05-02 23:57:27 +0900

    manifest 수정

---
+++ app/src/main/java/com/example/woohangsi_app/BudgetTotalEdit.java
@@ -0,0 +1,19 @@
+package com.example.woohangsi_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BudgetTotalEdit extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.total_budget_inquiry);
+    }
+}
